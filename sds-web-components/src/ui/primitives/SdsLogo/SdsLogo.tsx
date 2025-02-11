93e3ca89d466d9cb2f028b29a04219b156d244f1
Ralph, Lucas <lralph@onenorth.com>
2025-02-11 14:08:49 -0600

    feat: initial skeleton of component, and scss file

---
+++ sds-web-components/src/ui/primitives/SdsLogo/SdsLogo.tsx
@@ -0,0 +1,8 @@
+// import { FunctionalComponent, h } from '@stencil/core';
+
+// // TODO: once AnchorOrButton has been merged in, update this
+// interface SdsLogoProps = AnchorOrButtonProps;
+
+// export const SdsImage: FunctionalComponent<SdsLogoProps> = () =>  (
+//     <div> test </div>;
+// )
