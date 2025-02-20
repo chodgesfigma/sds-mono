cb74d7aafcf1c57177f53745ccd6dac6ee23c206
Ralph, Lucas <lralph@onenorth.com>
2025-02-20 14:34:08 -0600

    feat: working example in vue, no styles yet

---
+++ packages/sds-vue-components/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { SdsButton, SdsAccordion, SdsAccordionItem } from "./index";
+</script>
+
+<template>
+  <div>
+    <h1>Testing Vue Components</h1>
+    <SdsButton variant="primary">Test!</SdsButton>
+    <SdsAccordion>
+      <SdsAccordionItem summary="Item 1"> test item 1</SdsAccordionItem>
+      <SdsAccordionItem summary="Item 2"> test item 2</SdsAccordionItem>
+      <SdsAccordionItem summary="Item 3"> test item 3</SdsAccordionItem>
+    </SdsAccordion>
+  </div>
+</template>
---
+++ packages/sds-vue-components/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as StencilVueComponents from "../lib/components";
+import { defineCustomElements } from "../../sds-web-components/loader";
+
+// applyPolyfills().then(() => {
+defineCustomElements(window);
+// });
+
+const app = createApp(App);
+
+// Register all Stencil components globally
+Object.entries(StencilVueComponents).forEach(([name, component]) => {
+  app.component(name, component);
+});
+
+app.mount("#app");
