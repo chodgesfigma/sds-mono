853a31b47fca3f7e106f73efc8b1feb9bbdbe1e3
misramek_allegis <misramek@onenorth.com>
2025-02-25 18:19:12 -0500

    feat: angular output
    - added and configured @stencil/angular-output-target
    - created angular package, with component project and example consumer app
    - updated consumer app to ingest and use component library

---
+++ packages/sds-angular-components/projects/example-consumer-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'example-consumer-app';
+
+  onClick = () => {
+    alert('click');
+  };
+}
