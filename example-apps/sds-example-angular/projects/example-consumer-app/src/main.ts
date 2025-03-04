dd19947323daceb50ea12026ccdc62ef8dec07be
Ralph, Lucas <lralph@onenorth.com>
2025-03-04 12:42:33 -0600

    feat: further along in progress of implementation of example app for angular output, however am running into some injection error within browser log. I have created an example folder with example-apps, and have initialized an app with a project within it

--- example-apps/sds-example-angular/projects/example-consumer-app/src/main.ts
+++ example-apps/sds-example-angular/projects/example-consumer-app/src/main.ts
@@ -2,13 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 import { defineCustomElements } from 'sds-web-components/loader';
-
 import 'sds-web-components/dist/sds/sds.css';
-
-defineCustomElements(window);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
     ngZoneEventCoalescing: true,
   })
   .catch((err) => console.error(err));
+
+defineCustomElements(window);
